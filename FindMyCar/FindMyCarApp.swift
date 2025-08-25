35e86adb81fc007a71e657563716aaa1712d04ab
NoriDev <m1nthing2322@gmail.com>
2025-08-25 10:24:04 +0900

    Initial Commit

---
+++ FindMyCar/FindMyCarApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindMyCarApp.swift
+//  FindMyCar
+//
+//  Created by NoriDev on 8/25/25.
+//
+
+import SwiftUI
+
+@main
+struct FindMyCarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
