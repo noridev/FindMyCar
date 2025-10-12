7925d139cfc7127e239c16f2fdf89e32437d664c
NoriDev <m1nthing2322@gmail.com>
2025-10-12 17:19:10 +0900

    Revert "feat: 차량까지의 경로 표시 지원"
    This reverts commit bf5be5a4810f24c796907a15b15d78667e37792d.

--- FindMyCar/FindMyCarMapView.swift
+++ FindMyCar/FindMyCarMapView.swift
@@ -43,7 +43,8 @@ struct FindMyCarMapView: View {
                     }
 
                     if let route = routeToVehicle, showingRoute {
-                        MapPolyline(route)
+                        MapPolyline(route.polyline)
+                            .stroke(.blue, style: StrokeStyle(lineWidth: 6, lineCap: .round, lineJoin: .round))
                     }
                 }
                 .mapStyle(mapStyle)
